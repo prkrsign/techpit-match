74a6b424e52a2b9d48838df0be43f5f4fa0f7452
prkrsign <prkrsign@yahoo.co.jp>
2019-10-07 13:24:46 +0900

    add change image preview

--- app/assets/javascripts/edit_upload.js
+++ app/assets/javascripts/edit_upload.js
@@ -1,11 +1,11 @@
-$(document).on("change", "#file_photo", function(e) {
+$(document).on("change", "#file_change", function(e) {
     var reader;
     if (e.target.files.length) {
       reader = new FileReader;
       reader.onload = function(e) {
         var userThumbnail;
         userThumbnail = document.getElementById('thumbnail');
-        $("#userImgPreview").addClass("is-active");
+        $("#changeImgPreview").addClass("is-active");
         userThumbnail.setAttribute('src', e.target.result);
       };
       return reader.readAsDataURL(e.target.files[0]);
