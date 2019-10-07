5c7e1c0980dc3335c7232ddc0c3d995a3315e28e
prkrsign <prkrsign@yahoo.co.jp>
2019-10-07 19:15:04 +0900

    reaction setting and create controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: %i(index show)
+  resources :reactions, only: %i(create)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
