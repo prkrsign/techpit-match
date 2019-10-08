e93471fa67a9757b8cf9450f768bcf89297be4ea
prkrsign <prkrsign@yahoo.co.jp>
2019-10-08 10:28:36 +0900

    create Reaction-function

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,6 +1,7 @@
 class ReactionsController < ApplicationController
     def create
-        reactions = Reaction.find_or_initialize_by(to_user_id: params[:user_id], from_user_id: current_user.id)
+        # binding.pry
+        reaction = Reaction.find_or_initialize_by(to_user_id: params[:user_id], from_user_id: current_user.id)
         reaction.update_attributes(
             status: params[:reaction]
         )
